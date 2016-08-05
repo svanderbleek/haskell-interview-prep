2c26641895bd65efcb0e5363722324701a49e194
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2016-08-05 16:30:13 -0400

    Create parse.hs

---
+++ parse.hs
@@ -0,0 +1,63 @@
+import Text.Parsec
+  (
+    (<|>),
+    parseTest
+  )
+
+import Text.Parsec.String
+  (
+    Parser
+  )
+
+import Text.Parsec.Expr 
+  (
+    Operator(Infix),
+    Assoc(AssocLeft),
+    buildExpressionParser
+  )
+
+import Text.Parsec.Token 
+  (
+    makeTokenParser,
+    reservedOp,
+    parens,
+    integer
+  )
+
+import Text.Parsec.Language
+  (
+    emptyDef
+  )
+
+data Expression
+  = Addition Expression Expression
+  | Multiplication Expression Expression
+  | Value Integer
+  deriving (Show)
+
+tokenParser = makeTokenParser emptyDef
+
+operatorTable =
+  [[binaryOperator "*" Multiplication]
+  ,[binaryOperator "+" Addition]]
+
+binaryOperator symbol constructor =
+  Infix constructorParser AssocLeft
+  where
+    constructorParser = 
+      do
+        reservedOp tokenParser symbol
+        return constructor
+
+operatorParser = buildExpressionParser operatorTable expressionParser
+
+expressionParser =
+  parens tokenParser operatorParser
+  <|> Value <$> integer tokenParser
+
+-- want this to also work for "1*1+1*1" :(
+main =
+  sequence 
+    [parseTest expressionParser "(1*1+1*1)"
+    ,putStrLn ""
+    ,parseTest expressionParser "1*1+1*1"]
